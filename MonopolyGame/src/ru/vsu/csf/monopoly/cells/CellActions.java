e3a2f149126b9de7ccb72986cf9903308517a5c3
Rogachev Egor <55750592+288801@users.noreply.github.com>
2023-01-03 13:46:23 +0300

    Delete MonopolyGame directory

--- MonopolyGame/src/ru/vsu/csf/monopoly/cells/CellActions.java
+++
@@ -1,8 +0,0 @@
-package ru.vsu.csf.monopoly.cells;
-
-import ru.vsu.csf.monopoly.game.Game;
-import ru.vsu.csf.monopoly.objects.Player;
-
-public interface CellActions {
-    void makeMove(Player player, Game game);
-}
